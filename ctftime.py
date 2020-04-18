eb6883a365175abef06a97222608b6f93a187e1b
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-04-18 13:00:22 +0100

    Add team JSON decoding

--- ctftime.py
+++ ctftime.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from pprint import pformat
+from pprint import pformat, pprint
 
 URL = "https://ctftime.org/api"
 API_VERSION = "/v1"
@@ -10,8 +10,7 @@ API_URL = URL + API_VERSION
 def _append_slash(s: str):
     if s.endswith("/"):
         return s
-    else:
-        return s + "/"
+    return s + "/"
 
 
 class ShortTeam(object):
@@ -25,8 +24,7 @@ class ShortTeam(object):
     def decode(dct):
         if "team_id" in dct and "team_name" in dct and "points" in dct:
             return ShortTeam(dct["team_id"], dct["team_name"], dct["points"])
-        else:
-            return None
+        return None
 
     def __str__(self):
         return f"team_id: {self.team_id}\nteam_name: {self.team_name}\npoints: {self.points}\n"
@@ -44,8 +42,7 @@ class Top10(object):
             team = ShortTeam.decode(dct)
             if team is None:
                 return Top10(year, dct[year])
-            else:
-                return ShortTeam.decode(dct)
+            return ShortTeam.decode(dct)
 
         return _decode
 
@@ -53,7 +50,7 @@ class Top10(object):
         return f"year: {self.year}\ntop: {pformat(self.top)}"
 
 
-def top10(year: str = None):
+def top10(year: str = "2020"):
     url = "https://ctftime.org/api/v1/top/"
     if year is not None:
         url += year
@@ -106,7 +103,57 @@ def teams(limit: int = 10):
     return resp
 
 
-def team(team_id: str = None):
+class Rating(object):
+    def __init__(self, organizer_points, rating_points, rating_place):
+        super().__init__()
+        self.organizer_points = organizer_points
+        self.rating_points = rating_points
+        self.rating_place = rating_place
+
+    @staticmethod
+    def decode(dct):
+        if (
+            "organizer_points" in dct
+            and "rating_points" in dct
+            and "rating_place" in dct
+        ):
+            return Rating(
+                dct["organizer_points"], dct["rating_points"], dct["rating_place"]
+            )
+        return None
+
+
+class Team(object):
+    def __init__(self, _id, name, aliases, country, academic, rating):
+        super().__init__()
+        self.id = _id
+        self.name = name
+        self.aliases = aliases
+        self.country = country
+        self.academic = academic
+        self.rating = rating
+
+    @staticmethod
+    def decode(dct):
+        pprint(dct)
+        rating = Rating.decode(dct)
+        if rating is not None:
+            return rating
+        if "id" not in dct:
+            # hack to bypass the dumb use of year value as key
+            for v in dct:
+                return (v, dct[v])
+        return Team(
+            dct["id"],
+            dct["name"],
+            dct["aliases"],
+            dct["country"],
+            dct["academic"],
+            dct["rating"],
+        )
+
+
+def team(team_id: str):
     url = "https://ctftime.org/api/v1/teams/"
     if team_id is not None:
         url += team_id
@@ -118,7 +165,12 @@ def team(team_id: str = None):
             "User-Agent": "Mozilla/5.0",  # the API does not accept the default UA
         },
     )
+    print(json.loads(resp.content, object_hook=Team.decode))
     return resp
+
+
+print(team("1005"))
+print(team("112556"))
 
 
 def results(year: str = None):
